590afeff693f8872c06bf4a1fd95ca691f418195
Wasiq Qamar <wasiq.qbutt@gmail.com>
2021-01-31 21:06:44 +0500

    Functions Commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAhry7V1tcX_nl5acRAimZLB1-z_bWbGLo",
+    authDomain: "communaly.firebaseapp.com",
+    databaseURL: "https://communaly.firebaseio.com",
+    projectId: "communaly",
+    storageBucket: "communaly.appspot.com",
+    messagingSenderId: "520856247879",
+    appId: "1:520856247879:web:6bed82d8c7ac829c995b62",
+    measurementId: "G-319LKMLCHE"
+  };
